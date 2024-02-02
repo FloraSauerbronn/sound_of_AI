be2422d831531c3bd7c9375df9220a27df9f889a
FloraSauerbronn <flora.msauerbronn@gmail.com>
2024-02-02 16:14:43 -0300

    Minha rotina que fiz seguindo o tutorial deu errado, copiei e colei igual o git do valerio velardo e fiz salvei o meu com _FS

---
+++ cnn_VGG.py
@@ -0,0 +1,6 @@
+############################################################################################################
+#                                  Author: Flora Medeiros Sauerbronn                                       #
+#                                           Date: 02/02/2024                                               #
+#                    Creating a CNN for audio_classification with video turorial from Valerio Velardo      #
+#                                   Youtube channel the sound of AI                                        #
+############################################################################################################
